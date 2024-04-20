efa91d1133ccc4e79804dd42a5962b55007da7f2
unknown <mon19nek>
2024-04-20 14:47:36 +0700

    init

--- src/Components/BookEdit.jsx
+++ src/Components/BookEdit.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import { useState } from 'react'
-const BookEdit = ({book, onEdit}) => {
+const BookEdit = ({ book, onEdit }) => {
   const [title, setTitle] = useState(book.title)
   const [des, setDes] = useState(book.des)
   const handlChangeTitle = (e) => {
     setTitle(e.target.value);
-}
-const handlChangeDes = (e) => {
-  setDes(e.target.value);
-}
-const handleSubmit = (e) => {
-  e.preventDefault();
-  onEdit(book.id, {
-    title,
-    des,
-  })
-}
+  }
+  const handlChangeDes = (e) => {
+    setDes(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onEdit(book.id, {
+      title,
+      des,
+    })
+  }
+
+  const handleCancel = (e) => {
+    setTitle(book.title)
+    setDes(book.des)
+  }
+
+
   return (
     <div >
       <h3>Add a book</h3>
       <form onSubmit={handleSubmit}>
         <label htmlFor="">Title</label>
         <input onChange={handlChangeTitle}
-         type="text" 
-         id="title" 
-         name="title"
-         value={title}
-         />
-        <input onChange={handlChangeDes} 
-        type="text" 
-        id="des" 
-        name="des"
-        value={des}
+          type="text"
+          id="title"
+          name="title"
+          value={title}
         />
-        <input type="submit" value="Edit"/>
-        <button>Cancel</button>
-    </form>
+        <input onChange={handlChangeDes}
+          type="text"
+          id="des"
+          name="des"
+          value={des}
+        />
+        <input type="submit" value="Edit" />
+        <button onClick={handleCancel}>Cancel</button>
+      </form>
     </div>
   )
 }
